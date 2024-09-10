31f6bc6fb962736517bac95352331e517e478b34
Agustin Salinas <agustinsalinas@Agustins-MacBook-Pro.local>
2024-09-10 14:00:17 -0300

    fix: video component now using motion box

--- src/core/components/media/Video/Video.tsx
+++ src/core/components/media/Video/Video.tsx
@@ -2,8 +2,8 @@ import { Box } from "@chakra-ui/react";
 import { Filter, MotionBox } from "@components/foundations";
 import { MarplaCommonComponent } from "@commonTypes/MarplaCommonComponent";
 import { FC, useEffect, useRef } from "react";
-import { useInView } from "@react-spring/web";
 import { getMotionProps } from "@utils/getMotionProps";
+import { useCalculateNodeSize } from "@hooks/useCalculateNodeSize";
 
 export const Video: FC<MarplaCommonComponent & any> = ({
   width,
@@ -18,18 +18,20 @@ export const Video: FC<MarplaCommonComponent & any> = ({
   // 1.0 is normal speed
   // 0.5 is half speed (slower)
   // 2.0 is double speed (faster)
-  // -1.0 is backwards, normal speed
-  // -0.5 is backwards, half speed
   playbackRate = 1.0,
   ...props
 }) => {
   const { motionProps, rest } = getMotionProps(props);
+  const { ref: sizeRef, size } = useCalculateNodeSize({ formatToPixels: true });
   const ref: any = useRef(null);
-  const isInView = useInView(ref);
 
   useEffect(() => {
     const videoElement = ref.current;
+
+    if (!videoElement) return () => {};
+
     videoElement.playbackRate = playbackRate;
+
     if (!startTime || !endTime) return () => {};
 
     const handleTimeUpdate = () => {
@@ -49,37 +51,41 @@ export const Video: FC<MarplaCommonComponent & any> = ({
     return () => {
       videoElement.removeEventListener("timeupdate", handleTimeUpdate);
     };
-  }, [startTime, endTime]);
+  }, [startTime, endTime, ref.current]);
 
   return (
-    <>
+    <Box w={"100%"}>
+    {/* Override ChakraUI global mediaquery */}
       <style>
         {`
-          :where(img, video) {
+          :where(video) {
             max-width: 100%;
-            height: ${height ? height : 'auto'};
+            height: ${height ? height : "auto"};
           }
         `}
       </style>
-      <Filter effect={"linear-gradient"} {...motionProps}>
-        {/* //   <MotionBox width={width} height={height} {...motionProps}>
-    //     <Box width={width} height={height}> */}
-        <video
-          loop={loop}
-          autoPlay
-          muted
-          preload="auto"
-          playsInline
-          // playbackRate={playbackRate}
-          ref={ref}
-          src={src}
-          width={width}
-          height={height}
-          {...rest}
-        />
-        {/* //     </Box>
-    //   </MotionBox> */}
-      </Filter>
-    </>
+      {/* DUMB COMPONENT TO CALCULATE WIDTH */}
+      <Box ref={sizeRef} w={"100%"} />
+      {size.width && (
+        <MotionBox width={size.width} height={height} {...motionProps}>
+          <Filter effect={effect ?? "linear-gradient"}>
+            <Box width={size.width} height={height}>
+              <video
+                loop={loop}
+                autoPlay
+                muted
+                preload="auto"
+                playsInline
+                ref={ref}
+                src={src}
+                width={width}
+                height={height}
+                {...rest}
+              />
+            </Box>
+          </Filter>
+        </MotionBox>
+      )}
+    </Box>
   );
 };
